794332e4ab5764555d172df4e6c24887e3ff0d96
asalunkhe <asalunkhe.embd@gmail.com>
2018-01-11 14:30:55 +0530

    message about commit

--- main.c
+++ main.c
@@ -5,8 +5,7 @@ int main()
 {
 char data1[10]="akash";    
 char data2[10]="salunkhe";
-    printf("Hello world!\n");
- printf("Hello world!\n");
+    
     printf("Hello world!\n");
     printf("Hello world!\n");
     return 0;
